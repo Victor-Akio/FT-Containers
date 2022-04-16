f9f8f9c806df02d7df3fc24e3d8300cb745ed8ab
vminomiy <vminomiy@student.42sp.org.br>
2022-04-16 05:14:38 +0200

    AVL is almost ready

--- includes/AVL.hpp
+++ includes/AVL.hpp
@@ -6,7 +6,7 @@
 /*   By: vminomiy <vminomiy@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 20:52:56 by vminomiy          #+#    #+#             */
-/*   Updated: 2022/04/16 01:02:12 by vminomiy         ###   ########.fr       */
+/*   Updated: 2022/04/16 05:14:07 by vminomiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,9 +71,17 @@ namespace ft {
 			}
 			~AVL(void) {}
 			//	Getter of the node height 
-			int		getHeight(Node<value_type, Alloc>	*node) {
+			int		getHeight(Node<value_type, Alloc> *node) {
 				if (!node) return (0);
 				return ( node->height );
+			}
+			//	Assign all node subtrees in sequence.
+			void	assign(Node<value_type, Alloc> *node) {
+				if (!node) return ;
+				assign(node->left);
+				if (node->data)
+					insert(*(node->data));
+				assign(node->right);
 			}
 			/*-----------------------------------------------------------------------------------------------------------------------------*/
 			/*-----[ Rotation ]-----*/
@@ -142,8 +150,8 @@ namespace ft {
 				return (root);
 			}
 			//	Balance the current branch, if needed it will rotate the  nodes, to return to an AVL tree status.
-			//	The range of height of the AVL tree should be between "-1 to 1"
-			AvlNode<value_type, Alloc>		*balance(AvlNode<value_type, Alloc> *node, value_type const &key) {
+			//	The range of height of the AVL tree nodes should be between "-1 to 1", if higher it will right rotate and if it is lower than -1, left rotate it.
+			Node<value_type, Alloc>		*balance(Node<value_type, Alloc> *node, value_type const &key) {
 				if ((getHeight(node->right) - getHeight(node->left)) > 1) {
 					if (_comp(key.first, node->left->data->first)) return (rightRotate(node));
 					else {
@@ -160,12 +168,110 @@ namespace ft {
 				}
 				return (node);
 			}
+			//	Insert a node to the tree and rebalance if needed.
+			//	The insertion will be on the right node, if the current key is greater than the root key, or in the left node if the current key is lower than the root key.
+			Node<value_type, Alloc>		*insert(Node<value_type, Alloc> *node, value_type const &key) {
+				if (!node) return (newNode(key));
+				if (_comp(key.first, node->date->first)) {
+					node->left = insert(node->left, key);
+					node->left->parent = node;
+				} else if (_comp(node->data->first, key.first)) {
+					node->right = insert(node->right, key);
+					node->right->parent = node;
+				} else
+					return (node);
+				node->height = std::max(getHeight(node->left), getHeight(node->right)) + 1;
+				return (balance(node, key));
+			}
 			/*-----------------------------------------------------------------------------------------------------------------------------*/
 			/*-----[ Delete ]-----*/
+			//	Delete the root node, if there is one.
+			bool		del(value_type key) {
+				if (find(root, key)) {
+					this->root = this->remove(this->root, key);
+					return (true);
+				}
+				return (false);
+			}
+			//	Delete a node recursively form the last node to the root node:
+			//	- (1) Child Nodes receives a NULL;
+			//	- (2) In case of one Child node, the parent of the node, will become the parent of the child.
+			//	- (3) If thre is 2 child nodes, You should find the next child (Min of the right child) rotate the node and delete the child.
+			Node<value_type, Alloc>		*del(Node<value_type, Alloc> *node, value_type const &key) {
+				if (!node) return (node);						//(1)
+				if (_comp(key.first, node->data->first))
+					node->left = remove(node->left, key);
+				else if (_comp(node->data->first, key.first))
+					node->right = remove(node->right, key);
+				else if (node->data->first = key.first) {
+					if (!node->left || !node->right) {
+						Node<value_type, Alloc> *tmp = node->left ? node->left : node->right;
+						if (!tmp) {
+							tmp = node;
+							node = NULL;
+						} else {
+							Node<value_type, Alloc> *nodeParent = node->parent;		//(2)
+							*node = *tmp;
+							node->parent = nodeParent;
+						}
+						_alloc.deallocate(tmp->data, 1);
+						n_alloc.deallocate(tmp, 1);
+					}
+				} else {
+					Node<value_type, Alloc> *tmp = Min(node->right);				//(3)
+					_alloc.construct(node->data, *tmp->data);
+					node->right = remove(node->right, *tmp->data);
+				}
+				if (!node) return (node);											//After onde deletion, it updates the height and return the node.
+				node->height = std:max(getHeight(node->left), getHeight(node->right)) + 1;
+				return (balance(node, key));
+			}
 			/*-----------------------------------------------------------------------------------------------------------------------------*/
 			/*-----[ Element Access ]-----*/
+			//	Return a child node, if exists
+			Node<value_type, Alloc>		*find(Node<value_type, Alloc> *node, value_type const &key) const {
+				if (!node) return (NULL);
+				if (node->data && node->data->first == key.first) return (node);
+				if (_comp(key.first, node->data->first)) return (find(node->left, key));
+				else if (_comp(node->data->first, key.first)) return (find(node->right, key));
+				return (node);
+			}
+			/*	Return the next node:
+						- Return NULL if there is no next;
+						- If there is a right subtree, find the minimal node of the subtree;
+						- If there is only a left subtree, check if the parent is not the next node, instead of the left one.
+			*/		
+			Node<value_type, Alloc>		*next(value_type const &key) const {
+				Node<value_type, Alloc>		*node = find(root, key);
+				if (!node) return (NULL);
+				if (node->right != NULL) return (Min(node->right));
+				Node<value_type, Alloc>		*next = node->parent;
+				while (next != NULL && node == next->right) {
+					node = next;
+					next = next->parent;
+				}
+				node = next;
+				return (node);
+			}
+			/*	Return the next node:
+						- Return NULL if there is no next;
+						- If there is a left subtree, find the minimal node of the subtree;
+						- If there is only a right subtree, check if the parent is not the next node, instead of the right one.
+			*/
+			Node<value_type, Alloc>		*previous(value_type const &key) const {
+				Node<value_type, Alloc>		*node = find(root, key);
+				if (!node) return (NULL);
+				if (node->left != NULL) return (Max(node->right));
+				Node<value_type, Alloc>		*previous = node->parent;
+				while (previous != NULL && node == previous->left) {
+					node = previous;
+					previous = previous->parent;
+				}
+				node = previous;
+				return (node);
+			}
 			/*-----------------------------------------------------------------------------------------------------------------------------*/
-			/*-----[ Comparison Functions ]-----*/
+			/*-----[ Utility ]-----*/
 			//	Return the Max Node Element of the AVL tree
 			Node<value_type, Alloc>		*Max(Node<value_type, Alloc> *node) const {
 				if (!node) return (NULL);
@@ -181,6 +287,15 @@ namespace ft {
 				while (min->left)
 					min = min->left;
 				return (min);
+			}
+			//	From the end to begining, it will deallocate all nodes
+			void		PreOrder(Node<value_type, Alloc> *node) {
+				if (!node) return ;
+				PreOrder(node->left);
+				_alloc.deallocate(node->data, 1);
+				PreOrder(node->right);
+				n_alloc.deallocate(node, 1);
+				node = NULL;
 			}
 		private:
 			key_compare		_comp;
